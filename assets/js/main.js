75a60eb1af2840372408020657ab0a0cff2fdfa3
Cj Dupre <66892164+CjDupre@users.noreply.github.com>
2021-05-01 05:22:39 -0400

    Init

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+function hoverFeatureBox() {
+	document.getElementById("title").style.color = "#71bde4";
+	document.getElementById("feature-box").style.background = "transparent";
+	document.getElementById("feature-box").style.cursor = "pointer";
+	document.getElementById("feature-p").style.color = "#71bde4";
+}
+function OutHoverFeatureBox() {
+	document.getElementById("title").style.color = "#71bde4";
+	document.getElementById("feature-box").style.background =
+		"linear-gradient(#b392b2 0%, #766287 50%, #372d4c 100%)";
+	document.getElementById("feature-box").style.cursor = "pointer";
+	document.getElementById("feature-p").style.color = "#71bde4";
+}
+function hoverFeatureBox2() {
+	document.getElementById("title2").style.color = "#71bde4";
+	document.getElementById("feature-box2").style.background = "transparent";
+	document.getElementById("feature-box2").style.cursor = "pointer";
+	document.getElementById("feature-p2").style.color = "#71bde4";
+}
+function OutHoverFeatureBox2() {
+	document.getElementById("title2").style.color = "#71bde4";
+	document.getElementById("feature-box2").style.background =
+		"linear-gradient(#b392b2 0%, #766287 50%, #372d4c 100%)";
+	document.getElementById("feature-box2").style.cursor = "pointer";
+	document.getElementById("feature-p2").style.color = "#71bde4";
+}
+function hoverFeatureBox3() {
+	document.getElementById("title3").style.color = "#71bde4";
+	document.getElementById("feature-box3").style.background = "transparent";
+	document.getElementById("feature-box3").style.cursor = "pointer";
+	document.getElementById("feature-p3").style.color = "#71bde4";
+}
+function OutHoverFeatureBox3() {
+	document.getElementById("title3").style.color = "#71bde4";
+	document.getElementById("feature-box3").style.background =
+		"linear-gradient(#b392b2 0%, #766287 50%, #372d4c 100%)";
+	document.getElementById("feature-box3").style.cursor = "pointer";
+	document.getElementById("feature-p3").style.color = "#71bde4";
+}
